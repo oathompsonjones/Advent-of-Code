a1e07c13c9aeb1dd19df6a20fd6e95b5c7f651da
Ollie Jones <oathompsonjones@gmail.com>
2022-12-04 17:12:35 +0000

    Improve Day 4

--- 2022/Solutions/Day 4: Camp Cleanup.js
+++ 2022/Solutions/Day 4: Camp Cleanup.js
@@ -2,29 +2,21 @@
 const input = require("../Utils/GetInput")();
 
 const rangePairs = input.split("\n").map((line) => line.split(",").map((range) => range.split("-").map(Number)));
-const getRange = (start, end) => {
-    const range = [];
-    for (let i = start; i <= end; i++)
-        range.push(i);
-    return range;
-};
-const fullyEnclosedPairs = [];
-for (const [[start1, end1], [start2, end2]] of rangePairs) {
-    const range1 = getRange(start1, end1);
-    const range2 = getRange(start2, end2);
-    const firstIsEnclosed = range1.every((x) => range2.includes(x));
-    const secondIsEnclosed = range2.every((x) => range1.includes(x));
-    if (firstIsEnclosed || secondIsEnclosed)
-        fullyEnclosedPairs.push([[start1, end1], [start2, end2]]);
-}
-console.log(fullyEnclosedPairs.length);
 
-const overlapPairs = [];
+let pairsCount = 0;
 for (const [[start1, end1], [start2, end2]] of rangePairs) {
-    const range1 = getRange(start1, end1);
-    const range2 = getRange(start2, end2);
-    const isOverlap = range1.some((x) => range2.includes(x));
-    if (isOverlap)
-        overlapPairs.push([[start1, end1], [start2, end2]]);
+    const firstIsEnclosed = start1 >= start2 && start1 <= end2 && end1 <= end2;
+    const secondIsEnclosed = start2 >= start1 && start2 <= end1 && end2 <= end1;
+    if (firstIsEnclosed || secondIsEnclosed)
+        pairsCount++;
 }
-console.log(overlapPairs.length);
+console.log(pairsCount);
+
+pairsCount = 0;
+for (const [[start1, end1], [start2, end2]] of rangePairs) {
+    const start1IsInRange2 = start1 >= start2 && start1 <= end2;
+    const start2IsInRange1 = start2 >= start1 && start2 <= end1;
+    if (start1IsInRange2 || start2IsInRange1)
+        pairsCount++;
+}
+console.log(pairsCount);
